1d6b351ba55b2a73589477ed98e97de29ccccbbc
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-24 17:20:04 +0000

    pwa

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Yuri G
-// Created on: March 2023
+// Created on: May 2023
 // This file contains the JS functions for index.html
 
 "use strict"
