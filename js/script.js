e694e00520ce3be372e5f30f504c12fd198a67a7
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-24 17:09:20 +0000

    pwa

--- js/script.js
+++ js/script.js
@@ -4,6 +4,17 @@
 // Created on: March 2023
 // This file contains the JS functions for index.html
 
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-1-08-Yuri-Gabutero24//sw.js", {
+    scope: "/ICS2O-Unit-1-08-Yuri-Gabutero24/",
+  })
+}
+
 function myButtonClicked() {
   alert("Hello, World!")
 }
